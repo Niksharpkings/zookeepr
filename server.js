88507ab49ccfde259bc895e640861207bea092f2
Niksharpkings <67552318+Niksharpkings@users.noreply.github.com>
2022-04-01 14:23:38 -0700

    edit package.json

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 const { animals } = require('./data/animals');
 
 function filterByQuery(query, animalsArray) {
